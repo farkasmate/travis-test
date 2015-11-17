90266f4470f20726e05466d66b08ab87e255f354
Mate Farkas <mate.farkas@flexionmobile.com>
2015-11-17 14:24:02 +0000

    Numbered

--- src/main/java/y2k/travis/App.java
+++ src/main/java/y2k/travis/App.java
@@ -8,10 +8,10 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
-        System.out.println( "Hello World!" );
-        System.out.println( "Hello World!" );
-        System.out.println( "Hello World!" );
-        System.out.println( "Hello World!" );
+        System.out.println( "1: Hello World!" );
+        System.out.println( "2: Hello World!" );
+        System.out.println( "3: Hello World!" );
+        System.out.println( "4: Hello World!" );
+        System.out.println( "5: Hello World!" );
     }
 }
